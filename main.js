81ea5ba894386b245084f18430705556a8c7e5eb
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2023-08-15 20:13:14 -0300

    init extension

---
+++ main.js
@@ -0,0 +1,58 @@
+function createIcon() {
+  const icon = document.createElement("img");
+  icon.src = chrome.runtime.getURL("images/link.svg");
+  icon.style.width = "15px";
+  icon.style.height = "15px";
+  icon.style.marginLeft = "5px";
+
+  return icon;
+}
+
+function createButton() {
+  const button = document.createElement("button");
+  button.textContent = "Baixar";
+  button.className = "button-download";
+  button.style.background = "#ffffff1a";
+  button.style.border = "none";
+  /* button.style.color = "#f1f1f1"; */
+  button.style.color = "#0db6e9";
+  button.style.padding = "10px";
+  button.style.borderRadius = "15px";
+  button.style.margin = "0 10px";
+  button.style.cursor = "pointer";
+  button.style.textWrap = "nowrap";
+  button.style.whiteSpace = "nowrap";
+  button.style.fontWeight = "500";
+  button.style.fontFamily = "Roboto, Arial, sans-serif";
+  button.style.display = "flex";
+  button.style.alignItems = "center";
+  button.style.justifyContent = "center";
+
+  return button;
+}
+
+function createCopyButton() {
+  var copyButton = createButton();
+  const icon = createIcon();
+
+  copyButton.appendChild(icon);
+
+  copyButton.onclick = function () {
+    var tabUrl = window.location.href;
+    navigator.clipboard.writeText(tabUrl);
+
+    window.open(`itube-download-link://${tabUrl}`, "_blank");
+  };
+
+  var menuElement = document.querySelector(
+    "#top-level-buttons-computed.top-level-buttons.style-scope.ytd-menu-renderer"
+  );
+
+  menuElement.appendChild(copyButton);
+}
+
+document.addEventListener("yt-navigate-finish", function () {
+  setTimeout(() => {
+    createCopyButton();
+  }, 1500);
+});
